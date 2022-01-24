f419d838e0085ed33ea459d150210bc1f0ac1610
adammontlake <92934931+adammontlake@users.noreply.github.com>
2022-01-24 08:57:00 +0200

    Update deploy-sql-big-data-local.sh

--- deploy-sql-big-data-local.sh
+++ deploy-sql-big-data-local.sh
@@ -3,6 +3,27 @@
 #Allowed parameters to run script
 allowed_commands=["help","check_dependencies","create_cluster"]
 
+#Cluster and project name
+CLUSTER_NAME="bdcsql"
+
+#Cluster connection settings
+CLUSTER_TYPE="openshift-prod"	#openshift-prod or openshift-dev-test
+OC_CLUSTER_USERNAME="kubeadmin"
+OC_CLUSTER_PASSWORD=""
+APISERVER=""
+
+#Application credentials
+AZDATA_USERNAME=""
+AZDATA_PASSWORD=""
+
+#Registry settings
+REGISTRY=""
+REPOSITORY=""
+IMAGE_TAG=""
+
+#Storage type (use local-path for testing)
+STORAGE_CLASS=""
+
 help(){
 	echo "Script to deploy bdc on local OpenShift cluster"
 	echo "Enter 1 or more of the following options:"
@@ -38,10 +59,7 @@ check_dependencies(){
 }
 
 oc_login(){
-	read -p 'OC_CLUSTER_USERNAME (default: kubeadmin): ' OC_CLUSTER_USERNAME
-	read -sp 'OC_CLUSTER_PASSWORD: ' OC_CLUSTER_PASSWOR
-	read -p 'APISERVER: ' APISERVER
-	OC_CLUSTER_USERNAME=${OC_CLUSTER_USERNAME:-kubeadmin}
+	echo "Login"
 	oc login $APISERVER -u $OC_CLUSTER_USERNAME -p $OC_CLUSTER_PASSWORD
 }
 
@@ -63,22 +81,12 @@ create_binding(){
 }
 
 deploy_cluster(){
-	read -p 'AZDATA USERNAME (sqladmin): ' AZDATA_USERNAME
-	read -sp 'AZDATA PASSWORD (password): ' AZDATA_PASSWORD
-
-	AZDATA_USERNAME=${AZDATA_USERNAME:-sqladmin}
-	AZDATA_PASSWORD=${AZDATA_PASSWORD:-password}
-
 	export AZDATA_USERNAME=$AZDATA_USERNAME
 	export AZDATA_PASSWORD=$AZDATA_PASSWORD
 	export ACCEPT_EULA="Yes"
 	azdata bdc config init --source openshift-dev-test -p custom --accept-eula yes --force
 	
 	echo "Update config"
-	read -p 'Docker registry: ' REGISTRY
-	read -p 'Docker repository: ' REPOSITORY
-	read -p 'Image tag: ' IMAGE_TAG
-	read -p 'Storage class: ' STORAGE_CLASS
 	azdata bdc config replace -p custom/bdc.json -j metadata.name=$CLUSTER_NAME
 	azdata bdc config replace -p custom/control.json -j spec.docker.registry=$REGISTRY
 	azdata bdc config replace -p custom/control.json -j spec.docker.repository=$REPOSITORY
@@ -86,14 +94,13 @@ deploy_cluster(){
 	azdata bdc config replace -p custom/control.json -j spec.storage.data.className=$STORAGE_CLASS
 	azdata bdc config replace -p custom/control.json -j spec.storage.logs.className=$STORAGE_CLASS
 
+	echo "Deploy"
 	azdata bdc create --config-profile custom --accept-eula yes
 	azdata login -n $CLUSTER_NAME
 	azdata bdc endpoint list -o table
 }
 
 create_cluster(){
-	read -p 'CLUSTER NAME (default: bdcsql): ' CLUSTER_NAME
-	CLUSTER_NAME=${CLUSTER_NAME:-bdcsql}
 	check_dependencies
 	oc_login
 	Prepare_cluster
